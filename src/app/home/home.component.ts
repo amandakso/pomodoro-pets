2f4c554574bb673d5b609fa575b8f6facacece84
Amanda So <soamanda95@gmail.com>
2024-07-17 16:03:34 -0700

    create timers component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { TimersComponent } from '../timers/timers.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TimersComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
