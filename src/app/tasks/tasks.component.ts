77003e5724e9db33384a36e4b74a92f3e20f772f
Amanda So <soamanda95@gmail.com>
2024-09-03 15:19:10 -0700

    add task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { PomodoroService } from '../pomodoro.service';
 import { CommonModule } from '@angular/common';
+import { TaskComponent } from '../task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
