5c8d1d354d1bd9990c5141d522a3f0444227e3ab
Amanda So <soamanda95@gmail.com>
2024-09-03 12:08:00 -0700

    add tasks component to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TimersComponent } from '../timers/timers.component';
+import { TasksComponent } from '../tasks/tasks.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TimersComponent],
+  imports: [TimersComponent, TasksComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
