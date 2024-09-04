b08d7ee623d503f4a98972a2a1aa1ceda67ffba3
Amanda So <soamanda95@gmail.com>
2024-09-03 18:44:10 -0700

    create set up for add task feature

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { PomodoroService } from '../pomodoro.service';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 import { TaskComponent } from '../task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, TaskComponent],
+  imports: [CommonModule, MatIconModule, TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   pomodoroService = inject(PomodoroService);
+
+  addTask(description: string) {
+    console.log('TODO');
+    console.log(description);
+  }
 }
