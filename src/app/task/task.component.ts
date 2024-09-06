98e8e4c2196730755c1a59a49b089f7203e0687f
Amanda So <soamanda95@gmail.com>
2024-09-06 12:54:13 -0700

    create set up for edit task feature

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,20 +1,27 @@
 import { Component, Input, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Task } from '../task';
 import { PomodoroService } from '../pomodoro.service';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
-  @Input() task!: Task;
-
+  editable = false;
   pomodoroService = inject(PomodoroService);
+
+  @Input() task!: Task;
 
   completeTask(id: Date) {
     this.pomodoroService.changeDoneStatus(id);
+  }
+
+  updateTask(id: Date, description: string) {
+    console.log('id', id);
+    console.log('description', description);
   }
 }
