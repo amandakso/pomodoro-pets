b8d132ee9130266d4ef064cdbb19b176fca0f249
Amanda So <soamanda95@gmail.com>
2024-09-10 13:46:59 -0700

    add tasks menu

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,22 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { PomodoroService } from '../pomodoro.service';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
 import { TaskComponent } from '../task/task.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, MatIconModule, TaskComponent],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    TaskComponent,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+  ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
